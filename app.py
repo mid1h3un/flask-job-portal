d52f210c3d8255a8fcdf465f4ff73c9e741c0197
MIDHUN V <106835220+mid1h3un@users.noreply.github.com>
2025-04-24 13:46:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request, redirect, flash, session,url_for
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+load_dotenv()
 app=Flask(__name__)
-app.secret_key = "My_secret_key"
+app.secret_key = os.getenv('SECRET_KEY')
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'job'
-
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 mysql=MySQL(app)
 
 @app.route('/',methods=['GET'])
